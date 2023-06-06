1118718c04a0f4e095469a39e4adf24e9f9515ec
evanbacon <bacon@expo.io>
2023-06-05 20:21:11 -0500

    created widget, etc

---
+++ alpha/alphaBundle.swift
@@ -0,0 +1,17 @@
+//
+//  alphaBundle.swift
+//  alpha
+//
+//  Created by Evan Bacon on 6/5/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct alphaBundle: WidgetBundle {
+    var body: some Widget {
+        alpha()
+        alphaLiveActivity()
+    }
+}
