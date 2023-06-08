a389f88fb84f222bf6446805abfe9fabfa081427
evanbacon <bacon@expo.io>
2023-06-07 21:39:42 -0500

    Update withXcodeChanges.ts

--- widget-plugin/withXcodeChanges.ts
+++ widget-plugin/withXcodeChanges.ts
@@ -74,6 +74,84 @@ function isNativeTargetWidget(target: PBXNativeTarget) {
   return hasSwiftUI && hasWidgetKit;
 }
 
+function notificationServiceInfoPlist() {
+  return {
+    NSExtension: {
+      NSExtensionPointIdentifier: "com.apple.usernotifications.service",
+      NSExtensionPrincipalClass: "$(PRODUCT_MODULE_NAME).NotificationService",
+    },
+  };
+}
+
+function createNotificationServiceConfigurationList(
+  project: XcodeProject,
+  {
+    name,
+    cwd,
+    bundleId,
+    deploymentTarget,
+    currentProjectVersion,
+  }: XcodeSettings
+) {
+  const common: BuildSettings = {
+    CLANG_ANALYZER_NONNULL: "YES",
+    CLANG_ANALYZER_NUMBER_OBJECT_CONVERSION: "YES_AGGRESSIVE",
+    CLANG_CXX_LANGUAGE_STANDARD: "gnu++20",
+    CLANG_ENABLE_OBJC_WEAK: "YES",
+    CLANG_WARN_DOCUMENTATION_COMMENTS: "YES",
+    CLANG_WARN_QUOTED_INCLUDE_IN_FRAMEWORK_HEADER: "YES",
+    CLANG_WARN_UNGUARDED_AVAILABILITY: "YES_AGGRESSIVE",
+    CODE_SIGN_STYLE: "Automatic",
+    COPY_PHASE_STRIP: "NO",
+    CURRENT_PROJECT_VERSION: 1,
+    DEBUG_INFORMATION_FORMAT: "dwarf-with-dsym",
+    GCC_C_LANGUAGE_STANDARD: "gnu11",
+    GENERATE_INFOPLIST_FILE: "YES",
+    INFOPLIST_FILE: "echo/Info.plist",
+    INFOPLIST_KEY_CFBundleDisplayName: name,
+    INFOPLIST_KEY_NSHumanReadableCopyright: "",
+    IPHONEOS_DEPLOYMENT_TARGET: "16.4",
+    LD_RUNPATH_SEARCH_PATHS:
+      "$(inherited) @executable_path/Frameworks @executable_path/../../Frameworks",
+    MARKETING_VERSION: 1.0,
+    MTL_FAST_MATH: "YES",
+    PRODUCT_BUNDLE_IDENTIFIER: bundleId,
+    PRODUCT_NAME: "$(TARGET_NAME)",
+    SKIP_INSTALL: "YES",
+    SWIFT_EMIT_LOC_STRINGS: "YES",
+    SWIFT_OPTIMIZATION_LEVEL: "-Owholemodule",
+    SWIFT_VERSION: "5.0",
+    TARGETED_DEVICE_FAMILY: "1,2",
+  };
+  const debugBuildConfig = XCBuildConfiguration.create(project, {
+    name: "Debug",
+    buildSettings: {
+      ...common,
+      // Diff
+      MTL_ENABLE_DEBUG_INFO: "INCLUDE_SOURCE",
+      SWIFT_ACTIVE_COMPILATION_CONDITIONS: "DEBUG",
+      SWIFT_OPTIMIZATION_LEVEL: "-Onone",
+      DEBUG_INFORMATION_FORMAT: "dwarf",
+    },
+  });
+
+  const releaseBuildConfig = XCBuildConfiguration.create(project, {
+    name: "Release",
+    buildSettings: {
+      CLANG_ANALYZER_NONNULL: "YES",
+      ...common,
+    },
+  });
+
+  const configurationList = XCConfigurationList.create(project, {
+    buildConfigurations: [debugBuildConfig, releaseBuildConfig],
+    defaultConfigurationIsVisible: 0,
+    defaultConfigurationName: "Release",
+  });
+
+  return configurationList;
+}
+
 function createShareConfigurationList(
   project: XcodeProject,
   {
