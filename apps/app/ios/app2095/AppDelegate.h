9210dc22955a77647500c1246a448eaaefd8e328
evanbacon <bacon@expo.io>
2024-03-28 00:41:17 -0500

    add hierarchy action

--- apps/app/ios/app2095/AppDelegate.h
+++ apps/app/ios/app2095/AppDelegate.h
@@ -1,7 +1,7 @@
+#import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
+#import <Expo/Expo.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
+@interface AppDelegate : EXAppDelegateWrapper
 
 @end
---
+++ apps/app/targets/action-hierarchy/expo-target.config.js
@@ -0,0 +1,9 @@
+/** @type {import('@bacons/apple-targets').Config} */
+module.exports = {
+  type: "action",
+  name: "Hierarchy",
+  icon: "../../assets/action.png",
+  colors: {
+    TouchBarBezel: "#DB739C",
+  },
+};
