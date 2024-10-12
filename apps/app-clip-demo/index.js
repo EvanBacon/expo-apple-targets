73231865d51c606bb365dff56b533f52e65cc54f
evanbacon <bacon@expo.io>
2024-10-12 18:06:00 -0500

    running clip

--- apps/app-clip-demo/index.js
+++ apps/app-clip-demo/index.js
@@ -1,1 +1,8 @@
-// do nothing, this isn't a React app
+import { registerRootComponent } from "expo";
+
+import App from "./App.js";
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
