95170b3628bc26c0283d2ec2f66ddbec562a350a
evanbacon <bacon@expo.io>
2025-01-26 15:54:01 -0800

    add simplified control widget guide

--- apps/widget-demo/targets/widget/WidgetControl.swift
+++ apps/widget-demo/targets/widget/WidgetControl.swift
@@ -2,69 +2,38 @@ import AppIntents
 import SwiftUI
 import WidgetKit
 
+//@main
+//struct exportWidgets: WidgetBundle {
+//    var body: some Widget {
+//        widgetControl()
+//    }
+//}
+
+@available(iOS 18.0, *)
 struct widgetControl: ControlWidget {
-    static let kind: String = "com.developer.example.widget"
-
+    static let kind: String = "com.developer.bacon.widget"
     var body: some ControlWidgetConfiguration {
-        AppIntentControlConfiguration(
-            kind: Self.kind,
-            provider: Provider()
-        ) { value in
-            ControlWidgetToggle(
-                "Start Timer",
-                isOn: value.isRunning,
-                action: StartTimerIntent(value.name)
-            ) { isRunning in
-                Label(isRunning ? "On" : "Off", systemImage: "timer")
-            }
+      StaticControlConfiguration(kind: Self.kind) {
+        ControlWidgetButton(action: OpenAppIntent()) {
+          Label("BACON1", systemImage: "laurel.leading")
         }
-        .displayName("Timer")
-        .description("A an example control that runs a timer.")
+      }
+      .displayName("OPEN BACON")
+      .description("A an OPEN BACON control that runs a timer.")
     }
 }
 
-extension widgetControl {
-    struct Value {
-        var isRunning: Bool
-        var name: String
-    }
-
-    struct Provider: AppIntentControlValueProvider {
-        func previewValue(configuration: TimerConfiguration) -> Value {
-            widgetControl.Value(isRunning: false, name: configuration.timerName)
-        }
-
-        func currentValue(configuration: TimerConfiguration) async throws -> Value {
-            let isRunning = true // Check if the timer is running
-            return widgetControl.Value(isRunning: isRunning, name: configuration.timerName)
-        }
-    }
-}
-
-struct TimerConfiguration: ControlConfigurationIntent {
-    static let title: LocalizedStringResource = "Timer Name Configuration"
-
-    @Parameter(title: "Timer Name", default: "Timer")
-    var timerName: String
-}
-
-struct StartTimerIntent: SetValueIntent {
-    static let title: LocalizedStringResource = "Start a timer"
-
-    @Parameter(title: "Timer Name")
-    var name: String
-
-    @Parameter(title: "Timer is running")
-    var value: Bool
-
-    init() {}
-
-    init(_ name: String) {
-        self.name = name
-    }
-
-    func perform() async throws -> some IntentResult {
-        // Start the timer…
-        return .result()
+// This must be in both targets when `openAppWhenRun = true`
+// https://developer.apple.com/forums/thread/763851
+@available(iOS 18.0, *)
+struct OpenAppIntent: ControlConfigurationIntent {
+    static let title: LocalizedStringResource = "Launch App"
+    static let description = IntentDescription(stringLiteral: "Launch the app!")
+    static let isDiscoverable = false
+    static let openAppWhenRun: Bool = true
+    
+    @MainActor
+    func perform() async throws -> some IntentResult & OpensIntent {
+        return .result(opensIntent: OpenURLIntent(URL(string: "https://pillarvalley.expo.app/settings")!))
     }
 }
--- packages/apple-targets/src/config-plugin.ts
+++ packages/apple-targets/src/config-plugin.ts
@@ -1,5 +1,5 @@
 import { ConfigPlugin } from "@expo/config-plugins";
-import { sync as globSync } from "glob";
+import { globSync } from "glob";
 import path from "path";
 import chalk from "chalk";
 
