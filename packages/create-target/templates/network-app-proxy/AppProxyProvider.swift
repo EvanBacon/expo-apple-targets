12acdf287476503f6bcbc596fa17d013f4dbbc27
antoinedesp <114112600+antoinedesp@users.noreply.github.com>
2025-09-06 20:06:19 +0200

    templates: add NE App Proxy support

---
+++ packages/create-target/templates/network-app-proxy/AppProxyProvider.swift
@@ -0,0 +1,34 @@
+import NetworkExtension
+
+class AppProxyProvider: NEAppProxyProvider {
+
+    override func startProxy(options: [String : Any]? = nil, completionHandler: @escaping (Error?) -> Void) {
+        // Add code here to start the process of connecting the tunnel.
+    }
+    
+    override func stopProxy(with reason: NEProviderStopReason, completionHandler: @escaping () -> Void) {
+        // Add code here to start the process of stopping the tunnel.
+        completionHandler()
+    }
+    
+    override func handleAppMessage(_ messageData: Data, completionHandler: ((Data?) -> Void)?) {
+        // Add code here to handle the message.
+        if let handler = completionHandler {
+            handler(messageData)
+        }
+    }
+    
+    override func sleep(completionHandler: @escaping() -> Void) {
+        // Add code here to get ready to sleep.
+        completionHandler()
+    }
+    
+    override func wake() {
+        // Add code here to wake up.
+    }
+    
+    override func handleNewFlow(_ flow: NEAppProxyFlow) -> Bool {
+        // Add code here to handle the incoming flow.
+        return false
+    }
+}
